9149ab26961bd2d0290c1f9a1f40c5564b7ce40e
Mubeg <nikitadanilov.2001@mail.ru>
2022-06-10 02:59:33 +0300

    Add proc support

--- Makefile
+++ Makefile
@@ -3,8 +3,10 @@ all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
-test:
+test1:
 	sudo dmesg -C
 	sudo insmod test_task.ko
-	sudo rmmod test_task.ko
+	echo '10' > /proc/test_task
+	cat /proc/test_task
 	dmesg
+	sudo rmmod test_task
--- test_task.c
+++ test_task.c
@@ -1,18 +1,46 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mubeg");
 MODULE_DESCRIPTION("A loadable Linux module as AVSoft Test task.");
-MODULE_VERSION("0.01");
+MODULE_VERSION("0.02");
+
+static struct proc_dir_entry *proc;
+
+static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) {
+
+    printk( KERN_DEBUG "write handler\n");
+
+    return -1;
+}
+ 
+static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) {
+
+    printk( KERN_DEBUG "read handler\n");
+
+    return 0;
+}
+ 
+static struct file_operations myops = {
+
+    .owner = THIS_MODULE,
+    .read = myread,
+    .write = mywrite,
+
+};
 
 static int __init test_task_init(void) {
- printk(KERN_INFO "Hello, World!\n");
- return 0;
+
+    proc = proc_create("test_task",0666,NULL,&myops);
+
+    return 0;
 }
 static void __exit test_task_exit(void) {
- printk(KERN_INFO "Goodbye, World!\n");
+
+    proc_remove(proc);
 }
 
 module_init(test_task_init);
