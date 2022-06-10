7413b6ed871cb147614f57466666b4f0c56a6bf0
Mubeg <nikitadanilov.2001@mail.ru>
2022-06-10 04:24:47 +0300

    Fixed segfault + add comments

--- Makefile
+++ Makefile
@@ -3,14 +3,32 @@ all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
-test1:
+start_test:
 	sudo dmesg -C
 	sudo insmod test_task.ko
 
+test1:
+	make start_test
 	echo '1' > /proc/test_task
 	echo '2' > /proc/test_task
 	cat /proc/test_task
 	echo '3' > /proc/test_task
 	cat /proc/test_task
+	make end_test
 
+test2:
+	make start_test
+	echo "123456789" > /proc/test_task
+	cat /proc/test_task
+	make end_test
+
+test3:
+	make start_test
+	echo "12345678" > /proc/test_task
+	cat /proc/test_task
+	echo "" > /proc/test_task
+	make end_test
+
+
+end_test:
 	sudo rmmod test_task
--- test_task.c
+++ test_task.c
@@ -7,9 +7,9 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mubeg");
 MODULE_DESCRIPTION("A loadable Linux module as AVSoft Test task.");
-MODULE_VERSION("0.03");
+MODULE_VERSION("0.1");
 
-#define MAX_BUFF_SIZE 100
+#define MAX_BUFF_SIZE 10
 
 static char buff[MAX_BUFF_SIZE] = {};
 
@@ -17,30 +17,29 @@ static struct proc_dir_entry *proc_entry = NULL;
 
 static ssize_t write(struct file *file, const char __user *user_buff, size_t count, loff_t *offset) {
 
-    int len = strlen(buff);
-    printk(KERN_DEBUG "\nBuff before = {\n%s}\n", buff);
+    int len = strlen(buff);     // len = number of characters
 
-    if(count > MAX_BUFF_SIZE - len)
+    if(count > MAX_BUFF_SIZE - len - 1) // intended input > remaining space (MAX - len - '\0')
         return -EFAULT;
 
     if(copy_from_user(buff + len, user_buff, count))
         return -EFAULT;
 
-    printk(KERN_DEBUG "Buff after = %s\n\n", buff);
+    buff[len + count] = '\0';   // add '\0' after appending a string
 
     return count;
 }
  
 static ssize_t read(struct file *file, char __user *user_buff, size_t count, loff_t *offset) {
 
-    int len = strlen(buff) + 1;
-    if(*offset > 0 || count < len)
+
+    int len = strlen(buff) + 1;     // len = number of characters + '\0'
+    if(*offset > 0 || count < len)  // no_offset or output buffer < intended output
         return 0;
     
     if(copy_to_user(user_buff, buff, len))
         return -EFAULT;
 
-    printk(KERN_DEBUG "len = %d\n", len);
     *offset = len;
     return len;
 }
